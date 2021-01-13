f3d4dae3dd46e4efc4a6629c421e48236ce2561b
Glitch (lateral-foamy-supply) <none>
2021-01-13 13:56:05 +0000

    🚿🌍 Checkpoint ./commands/afk.js:26594853/132 ./commands/invite.js:26594853/247

--- commands/afk.js
+++ commands/afk.js
@@ -1,19 +1,19 @@
 const { MessageEmbed } = require("discord.js");
 const sendError = require("../util/error");
-const fs = require('fs');
+const fs = require('fs');//youtube/NoblesYT
 
 
 module.exports = {
   info: {
     name: "afk",
-    description: "24/7",
+    description: "24/7",//youtube/NoblesYT
     usage: "[afk]",
     aliases: ["24/7"],
   },
 
   run: async function (client, message, args) {
     let afk = JSON.parse(fs.readFileSync("./afk.json", "utf8"));
-       if (!afk[message.guild.id]) afk[message.guild.id] = {
+       if (!afk[message.guild.id]) afk[message.guild.id] = {//youtube/NoblesYT
         afk: false,
     };
     var serverQueue = afk[message.guild.id]
@@ -22,13 +22,13 @@ module.exports = {
              message.channel.send({
                 embed: {
                     color: "GREEN",
-                    description: `💤  **|**  AFK is **\`${serverQueue.afk === true ? "enabled" : "disabled"}\`**`
+                    description: `💤  **|**  AFK is **\`${serverQueue.afk === true ? "enabled" : "disabled"}\`**`//youtube/NoblesYT
                 }
             });
             return  fs.writeFile("./afk.json", JSON.stringify(afk), (err) => {
         if (err) console.error(err);
     });
         };
-    return sendError("There is nothing playing in this server.", message.channel);
+    return sendError("There is nothing playing in this server.", message.channel);//youtube/NoblesYT
   },
 };
