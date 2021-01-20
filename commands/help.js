914956481455be0763a8e83f3876b2faaf90e506
Glitch (lateral-foamy-supply) <none>
2021-01-20 17:14:10 +0000

    👾🎭 Checkpoint ./commands/help.js:6373742/134 ./package.json:6373742/947

--- commands/help.js
+++ commands/help.js
@@ -13,14 +13,14 @@ module.exports = {
 
         client.commands.forEach(cmd => {
             let cmdinfo = cmd.info
-            allcmds+="``"+client.config.prefix+cmdinfo.name+" "+cmdinfo.usage+"`` ~ "+cmdinfo.description+"\n"
+            allcmds+="``"+process.env.PREFIX+cmdinfo.name+" "+cmdinfo.usage+"`` ~ "+cmdinfo.description+"\n"
         })
 
         let embed = new MessageEmbed()
         .setAuthor("Commands of "+client.user.username, "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
         .setColor("BLUE")
         .setDescription(allcmds)
-        .setFooter(`To get info of each command you can do ${client.config.prefix}help [command] | Maked by Nobles#3226`)
+        .setFooter(`To get info of each command you can do ${process.env.PREFIX}help [command] | Maked by Nobles#3226`)
 
         if(!args[0])return message.channel.send(embed)
         else {
@@ -34,7 +34,7 @@ module.exports = {
             .setDescription(`
 Name: ${command.info.name}
 Description: ${command.info.description}
-Usage: \`\`${client.config.prefix}${command.info.name} ${command.info.usage}\`\`
+Usage: \`\`${process.env.PREFIX}${command.info.name} ${command.info.usage}\`\`
 Aliases: ${command.info.aliases.join(", ")}
 `)
             message.channel.send(commandinfo)
