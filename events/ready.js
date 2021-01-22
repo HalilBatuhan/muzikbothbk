ef5f1e7c1fbb5451b85ab946c54edcabf789532a
Glitch (lateral-foamy-supply) <none>
2021-01-22 09:28:41 +0000

    🏬🐘 Checkpoint ./events/ready.js:26519553/52

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {//youtube/NoblesYT
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Youtube/NoblesYT", { //Oynuyor Kısmı
+  await client.user.setActivity("youtube 𝖖𝖚𝖆𝖗𝖙𝖊𝖝", { //Oynuyor Kısmı
     type: "WATCHING",//LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
