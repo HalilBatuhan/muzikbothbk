f53a4f67120048c56c0c8969ca1cfaec46f1eb7e
Glitch (lateral-foamy-supply) <none>
2021-01-22 07:11:09 +0000

    📮👻 Checkpoint ./commands/invite.js:26519553/331

--- commands/invite.js
+++ commands/invite.js
@@ -12,7 +12,7 @@ module.exports = {
     
     let invite = new MessageEmbed()
     .setTitle(`Invite ${client.user.username}`)
-    .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot)`)
+    .setDescription(`Want me in your server? Invite me today! \n\n [Invite Link](https://discord.com/api/oauth2/authorize?client_id=802070154959781929&permissions=8&redirect_uri=https%3A%2F%2Fglitch.com%2Fedit%2F%23%21%2Fjungle-orange-jupiter%3Fpath%3D.env%253A4%253A0&response_type=code&scope=bot%20applications.builds.read)`)
     .setColor("BLUE")
     return message.channel.send(invite);
   },
